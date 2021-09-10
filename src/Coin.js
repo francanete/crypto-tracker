dfe8165c18dcc608d1b5f5e382e49ce3a790548b
Fran Canete <francanetedev@gmail.com>
2021-09-10 17:33:50 +0100

    Add format to the price value

--- src/Coin.js
+++ src/Coin.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
+function currencyFormat(num) {
+  return '£' + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+
+
 const Coin = ({ name, image, symbol, price, volume, priceChange, marketcap }) => {
   return (
     <div className="coin-container">
@@ -10,7 +16,7 @@ const Coin = ({ name, image, symbol, price, volume, priceChange, marketcap }) =>
           <p className="coin-symbol">{symbol}</p>
         </div>
         <div className="coin-data">
-          <p className="coin-price">£{price}</p>
+          <p className="coin-price">{currencyFormat(price)}</p>
           <p className="coin-volume">£{volume.toLocaleString()}</p>
           { priceChange < 0 ? 
             ( <p className="coin-percent red">{priceChange.toFixed(2)}%</p>) :
