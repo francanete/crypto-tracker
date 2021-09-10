b7c2c1157bbdeca4295a3d707fae88bc53a20743
Fran Canete <francanetedev@gmail.com>
2021-09-10 13:51:56 +0100

    Update currency and chage input style

--- src/Coin.js
+++ src/Coin.js
@@ -10,8 +10,8 @@ const Coin = ({ name, image, symbol, price, volume, priceChange, marketcap }) =>
           <p className="coin-symbol">{symbol}</p>
         </div>
         <div className="coin-data">
-          <p className="coin-price">${price}</p>
-          <p className="coin-volume">${volume.toLocaleString()}</p>
+          <p className="coin-price">£{price}</p>
+          <p className="coin-volume">£{volume.toLocaleString()}</p>
           { priceChange < 0 ? 
             ( <p className="coin-percent red">{priceChange.toFixed(2)}%</p>) :
             (<p className="coin-percent green">{priceChange.toFixed(2)}%</p>) 
